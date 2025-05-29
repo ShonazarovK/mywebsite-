e0e6e586a78db6f31569e8071233af77cf5aed31
Elbek <azimjonovelbek11@gmail.com>
2025-05-30 03:39:10 +0500

    Fixed missing view: removed per_product_view

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,19 +1,21 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
-from .views import (inventory_list,
-                    per_product_view,
-                    add_product,
-                    delete_inventory,
-                    update_inventory,
-                    dashboard)
+from .views import (
+    inventory_list,
+    add_product,
+    delete_product,
+    update_product,
+    dashboard,
+    product_detail
+)
 
 urlpatterns = [
     path("", inventory_list, name='inventory_list'),
-    path("per_product/<int:pk>", per_product_view, name='per_product'),
+    path("product/<int:pk>/", product_detail, name='product_detail'),
     path("add_inventory/", add_product, name='add_inventory'),
-    path("delete/<int:pk>", delete_inventory, name='delete_inventory'),
-    path("update/<int:pk>", update_inventory, name='update_inventory'),
+    path("delete/<int:pk>/", delete_product, name='delete_inventory'),
+    path("update/<int:pk>/", update_product, name='update_inventory'),
     path("dashboard/", dashboard, name="dashboard"),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
