b5c7dbc32cb580eccc0b0616d1dff76481f1e733
Elbek <azimjonovelbek11@gmail.com>
2025-05-30 13:39:18 +0500

    Add category field to Inventory model

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 import datetime
 
-
 class Inventory(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, unique=True)
     cost_per_item = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
@@ -10,6 +9,9 @@ class Inventory(models.Model):
     sales = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
     stock_date = models.DateField(auto_now=True)
     last_sales_date = models.DateField(auto_now=True)
+    
+    category = models.CharField(max_length=100, null=True, blank=True, help_text="Kategoriya nomini kiriting")
 
     def __str__(self):
         return self.name
+
