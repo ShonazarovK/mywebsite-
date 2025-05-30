d180b4d298017a0571c7f19dd5bd333b87c84a8b
Elbek <azimjonovelbek11@gmail.com>
2025-05-30 16:48:00 +0500

    fix: correct update view function name to inventory_update

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,19 +4,18 @@ from django.views.generic.base import RedirectView
 from .views import (
     inventory_list,
     product_detail,
-    add_product,  # ✅ to'g'ri nom
+    add_product,
     delete_product,
-    update_product,
+    inventory_update,  # ✅ TO‘G‘RI NOM
     dashboard,
 )
-
 
 urlpatterns = [
     path("", inventory_list, name='inventory_list'),
     path("product/<int:pk>/", product_detail, name='product_detail'),
     path("add_inventory/", add_product, name='add_inventory'),
     path("delete/<int:pk>/", delete_product, name='delete_inventory'),
-    path("update/<int:pk>/", update_product, name='update_inventory'),
+    path("update/<int:pk>/", inventory_update, name='update_inventory'),  # ✅ TO‘G‘RI QATOR
     path("dashboard/", dashboard, name="dashboard"),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
